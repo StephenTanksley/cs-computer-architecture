e3cfe9c8ddf61560d9c8b121a951a4b0b3771d8e
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-09-02 18:21:39 -0500

    I think I'm doing this right? I'm gonna ask the help channel.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,29 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+
+        # general purpose registers for storing information we're working with.
+        self.R0 = [0] * 8
+        self.R1 = [0] * 8
+        self.R2 = [0] * 8
+        self.R3 = [0] * 8
+        self.R4 = [0] * 8
+
+        # Reserved as the interrupt mask (IM)
+        self.R5 = [0] * 8
+
+        # Reserved as interrupt status (IS)
+        self.R6 = [0] * 8
+
+        # Reserved as stack pointer (SP)
+        self.R7 = [0] * 8
+        
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +35,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +64,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
